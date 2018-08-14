baca76c4f767ba04780a10d20b890eecb288d948
Ubuntu <ubuntu@ip-172-31-18-255.ap-northeast-2.compute.internal>
2018-08-14 10:31:26 +0000

    한글변환 스트링 전송

--- video_streaming.py
+++ video_streaming.py
@@ -3,6 +3,8 @@ from flask_cors import CORS
 from yolo import detect_image
 import os
 
+translateEtoK = { 'person':'사람', 'bicycle':'자전거', 'car':'자동차', 'motorbike':'오토바이', 'aeroplane':'비행기', 'bus':'버스', 'train':'전철', 'truck':'트럭', 'boat':'보트', 'traffic light':'신호등', 'fire hydrant':'소화전', 'stop sign':'정지 표지판', 'parking meter':'계량기', 'bench':'벤치', 'bird':'새', 'cat':'고양이', 'dog':'개', 'horse':'말', 'sheep':'양', 'cow':'소', 'elephant':'코끼리', 'bear':'곰', 'zebra':'얼룩말', 'giraffe':'기린', 'backpack':'가방', 'umbrella':'우산', 'handbag':'핸드백', 'tie':'넥타이', 'suitcase':'서류가방', 'frisbee':'원반', 'skis':'스키', 'snowboard':'스노우보드', 'sports ball':'공', 'kite':'연', 'baseball bat':'방망이', 'baseball glove':'장갑', 'skateboard':'스케이트보드', 'surfboard':'서핑보드', 'tennis racket':'테니스라켓', 'bottle':'유리병', 'wine glass':'유리컵', 'cup':'컵', 'fork':'포크', 'knife':'나이프', 'spoon':'숟가락', 'bowl':'그릇', 'banana':'바나나', 'apple':'사과', 'sandwich':'샌드위치', 'orange':'오렌지', 'broccoli':'브로콜리', 'carrot':'당근', 'hot dog':'핫도그', 'pizza':'피자', 'donut':'도넛', 'cake':'케이크', 'chair':'의자', 'sofa':'소파', 'pottedplant':'화분', 'bed':'침대', 'diningtable':'식탁', 'toilet':'화장실', 'tvmonitor':'모니터', 'laptop':'노트북', 'mouse':'마우스', 'remote':'리모컨', 'keyboard':'키보드', 'cell phone':'핸드폰', 'microwave':'전자렌지', 'oven':'오븐', 'toaster':'토스터기', 'sink':'싱크대', 'refrigerator':'냉장고', 'book':'책', 'clock':'시계', 'vase':'병', 'scissors':'가위', 'teddy bear':'곰인형', 'hair drier':'헤어드라이어', 'toothbrush':'칫솔' }
+
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 #cors = CORS(app, resources={r"/*": {"origins": "http://121.129.2.195:8080"}})
@@ -37,11 +39,19 @@ def test():
     file = request.files['abc']
     filename = file.filename
     file.save('/home/ubuntu/image.jpg')
-    result = image_detect('/home/ubuntu/image.jpg')
+    result = detect_image('/home/ubuntu/image.jpg')
+    str_result = ''
+
+    for i in result:
+        str_result=str_result + translateEtoK.get(i['class'], '') + ' '
+
+    str_result = str_result + '있습니다'
+
     data = {
         'status': 200
     }
-    return Response(status=200, mimetype='application/json')
+    print(str_result)
+    return Response(str_result,status=200, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)

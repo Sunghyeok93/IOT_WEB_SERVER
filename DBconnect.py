bb3cc3ae17892e16985dea385e69e299d533e1f2
einhugo <aeinhugo@gmail.com>
2018-08-20 08:40:19 +0000

    db 테스트전

--- DBconnect.py
+++ DBconnect.py
@@ -1,11 +1,35 @@
 import sqlite3
 
+
+
 class DBconnect():
 
-    def __init__():
-        con = sqlite3.connect('./test.db')
-        cur = con.cursor()
-        cur.execute('SELECT * FROM PhoneBook')
-        for row in cur:
-            print(row)
+    def __init__(self):
+        self.connect = sqlite3.connect('./iot.db')
+
+
+    def insertMessage(time, content, sender):
+        cursor = self.connect.cursor()
+        query = "INSERT INTO Message(time, content, sender) Values(" + time + "," + content + "," + sender + ");"
+        return cursor.execute(query)
+
+    
+    def insertImage(time, path, size):
+        cursor = self.connect.cursor()
+        query = "INSERT INTO Image(time, path, size) Values(" + time + "," + path + "," + size + ");"
+        return cursor.execute(query)
+
+    
+    def selectMessage():
+        cursor = self.connect.cursor()
+        query = "SELECT * FROM Message"
+        cursor.execute(query)
+        return cursor.fetchall()
+
+
+    def selectMessage():
+        cursor = self.connect.cursor()
+        query = "SELECT * FROM Image"
+        cursor.execute(query)
+        return cursor.fetchall()
 

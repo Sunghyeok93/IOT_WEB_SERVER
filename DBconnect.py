1f18b042ca0b34f85b34846e53f9b1786e709394
einhugo <aeinhugo@gmail.com>
2018-08-31 11:45:16 +0000

    DBconnect Message 테이블 함수 구현

--- DBconnect.py
+++ DBconnect.py
@@ -7,20 +7,34 @@ class DBconnect():
         self.cursor = self.connect.cursor()
 
     def insertMessage(self, time, content, sender):
-        query = "INSERT INTO Message(time, content, sender) Values(" + time + "," + content + "," + sender + ");"
+        isRead = 0
+        if sender is not "ARTIK":
+            isRead = 1
+        query = "INSERT INTO Message(time, content, sender, isRead) Values(" + time + "," + content + "," + sender + "," + isRead + ");"
         return self.cursor.execute(query)
 
-    
+    def getMessageNotRead(self):
+        query = "SELECT num FROM Message WHERE isRead = 0;"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def modifyMessageIsRead(self, num, isRead):
+        query = "UPDATE Message SET isRead = " + isRead + " WHERE num = " + num + ";"
+        return 0;    
+
+    def getMessageContent(self, num):
+        query = "SELECT content FROM Message WHERE isRead = 0 ORDER BY time LIMIT 1 OFFSET 0;"
+        self.cursor.execute(query)
+        return self.cursor.fetchone()
+
     def insertImage(self, time, path, size):
         query = "INSERT INTO Image (time, path, size) VALUES ('" + time + "','" + path + "','" + size + "');"
         return self.cursor.execute(query)
-
-    
+   
     def selectMessage(self):
         query = "SELECT * FROM Message"
         self.cursor.execute(query)
         return self.cursor.fetchall()
-
 
     def selectPhoto(self):
         query = "SELECT * FROM Image"
